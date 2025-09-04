504c875d3c0e93af0e3c5fd5c9e81d2602111abf
Kovath <Kovath@users.noreply.github.com>
2025-09-04 01:56:34 -0700

    renamed to api and nuked it down

---
+++ api/index.ts
@@ -0,0 +1,40 @@
+import express, { Express, Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routers
+
+// error handling
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Express + TypeScript Server' });
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server running at http://localhost:${port}`);
+});
+
+export default app;
